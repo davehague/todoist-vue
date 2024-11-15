ed7a63b8e092b7af8fac548136dca5ce5f75ff83
David Hague <david.hague@gmail.com>
2024-11-15 11:59:18 -0500

    Styling issues, don't scroll left and right on mobile, make links more obvious in the title

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto">
+  <div class="flex-1 overflow-y-auto overflow-x-hidden">
     <div class="space-y-2 pt-4 max-w-[800px] w-full mx-auto">
       <TaskItem
         v-for="task in tasks"
--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -6,10 +6,10 @@ export const renderMarkdown = (content: string) => {
   try {
     const renderer = new marked.Renderer();
     renderer.link = ({ href, tokens }) => {
-      const fullTitle = tokens
+      const title = tokens
         .map((t: Token) => (t as Tokens.Text).text)
         .join(",");
-      return `<a href="${href}" target="_blank" rel="noopener noreferrer" class="text-blue-600 dark:text-blue-400 hover:underline">${fullTitle}</a>`;
+      return `<a href="${href}" target="_blank" rel="noopener noreferrer" class="text-blue-600 dark:text-blue-300 hover:underline">${title}</a>`;
     };
 
     return marked(content, { renderer });
